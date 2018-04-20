e5d34d69c7e51b4fd8349aa6a33a79cc6d3447d2
松村敬太 <MatsumuraKeita@matsumuratakashifutoshi-no-imac.local>
2018-04-21 00:37:05 +0900

    init Commit

--- diceTest/ViewController.swift
+++ diceTest/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceView: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func tumblingDice(_ sender: Any) {
     }
 
     override func didReceiveMemoryWarning() {
