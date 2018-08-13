96a7917c3df7cb4e0128637a76c155ad8f2cf379
MatsumuraKeita <matsumurakeita@matsumurakeitanomacbook-puro.local>
2018-08-13 13:57:55 +0900

    サイコロの画像を使うように変更(サイズ、背景などは未対応)

--- diceTest/ViewController.swift
+++ diceTest/ViewController.swift
@@ -10,7 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var diceView: UILabel!
+    var img1 :UIImage = UIImage(named:"sai1.gif")!
+    var img2 :UIImage = UIImage(named:"sai2.gif")!
+    var img3 :UIImage = UIImage(named:"sai3.gif")!
+    var img4 :UIImage = UIImage(named:"sai4.gif")!
+    var img5 :UIImage = UIImage(named:"sai5.gif")!
+    var img6 :UIImage = UIImage(named:"sai6.gif")!
+    
+    var imgArray:[UIImage] = []
+//    @IBOutlet weak var diceView: UILabel!
+    
+    @IBOutlet weak var diceView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,9 +29,12 @@ class ViewController: UIViewController {
     @IBAction func tumblingDice(_ sender: Any) {
         //rundomな値を生成
         let rundomNum = arc4random_uniform(6)
-        let diceNum = rundomNum + 1
+        let diceNum = Int(rundomNum)
         //diceViewに代入
-        diceView.text = String(diceNum)
+//        diceView.text = String(diceNum)
+        //diceNumに応じて画像を切り替える
+        imgArray = [img1,img2,img3,img4,img5,img6]
+        diceView.image = imgArray[diceNum]
     }
 
     override func didReceiveMemoryWarning() {
